e203beef11044ced863f32e3b2fcced2a7669dbe
skpatil99 <skpatil705@gmail.com>
2024-09-10 03:00:35 -0400

    read account compelted

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,23 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_read_an_account(self):
+        account = self._create_accounts(1)[0]
+        print(account.id)
+        url = "{0}/{1}".format(BASE_URL,account.id)
+        print(url)
+        resp = self.client.get(str(url), content_type="application/json")
+        print(resp)
+        print(account.name)
+        self.assertEqual(resp.status_code,status.HTTP_200_OK)
+
+        data = resp.get_json()
+        print(data)
+        self.assertEquals(data["name"],account.name)
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
